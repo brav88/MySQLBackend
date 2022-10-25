19cf2dc29d26c71e277356377babd2dc4c94cb49
brav88 <brav88@hotmail.com>
2022-10-24 21:05:58 -0600

    Agregar archivos de proyecto.

---
+++ MySQLBackend/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySQLBackend.Models;
+using System.Diagnostics;
+
+namespace MySQLBackend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DatabaseHelper.DatabaseHelper.InsertUser(new Models.User()
+            {
+                Name = "Jennifer",
+                LastName = "Johnson",
+                Address = "New York",
+                Email = "jen.johnson@gmail.com",
+                Phone = 64646464,
+                DateIn = DateTime.Now
+            });
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MySQLBackend/DatabaseHelper/DatabaseHelper.cs
@@ -0,0 +1,95 @@
+﻿using MySql.Data.MySqlClient;
+using MySQLBackend.Models;
+using System.Data;
+
+namespace MySQLBackend.DatabaseHelper
+{
+    public class DatabaseHelper
+    {
+        const string user = "root";
+        const string password = "Admin$1234";
+        const string servidor = @"localhost";
+        const string port = "3306";
+        const string baseDatos = "mydatabase";
+        const string strConexion = $"server={servidor};Port={port};uid={user};pwd={password};database={baseDatos}";
+
+        public static void InsertUser(User user)
+        {
+            List<MySqlParameter> paramList = new List<MySqlParameter>()
+            {
+                new MySqlParameter("pName", user.Name),
+                new MySqlParameter("pLastName",user.LastName),
+                new MySqlParameter("pEmail",user.Email),
+                new MySqlParameter("pPhone",user.Phone),
+                new MySqlParameter("pAddress",user.Address),
+                new MySqlParameter("pDateIn",user.DateIn)
+            };
+
+            ExecStoreProcedure("spInsertUser", paramList);
+        }
+
+        //Para select 
+        public static DataTable ExecuteStoreProcedure(string procedure, List<MySqlParameter> param)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(strConexion))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.CommandText = procedure;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = conn;
+
+                    if (param != null)
+                    {
+                        foreach (MySqlParameter item in param)
+                        {
+                            cmd.Parameters.Add(item);
+                        }
+                    }
+
+                    cmd.ExecuteNonQuery();
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    return dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void ExecStoreProcedure(string procedure, List<MySqlParameter> param)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(strConexion))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.CommandText = procedure;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = conn;
+
+                    if (param != null)
+                    {
+                        foreach (MySqlParameter item in param)
+                        {
+                            cmd.Parameters.Add(item);
+                        }
+                    }
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
